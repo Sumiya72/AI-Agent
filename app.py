05a3e9829ebac6bc96cbdb07c1f0aff52872e623
Sumiya72 <itzsamiya2000000p0p@gmail.com>
2026-05-11 14:04:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import streamlit as st
 from agent import create_agent
 
 st.set_page_config(page_title="AI Agent 🤖")
+
 st.title("🤖 LangChain + Groq AI Agent")
 
-if "agent" not in st.session_state:
-    st.session_state.agent = create_agent()
+agent = create_agent()
 
-user_input = st.text_input("Ask anything:")
+user_input = st.text_input("Ask something:")
 
 if st.button("Send"):
     if user_input:
-        result = st.session_state.agent.invoke({"input": user_input})
-        st.write("### 🤖 Agent Response:")
-        st.write(result["output"])
+        response = agent.invoke({"input": user_input})
+        st.write("### 🤖 Response:")
+        st.write(response)
