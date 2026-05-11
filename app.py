735996502fbbddf87117e165d5a95b3e1b38677c
Sumiya72 <itzsamiya2000000p0p@gmail.com>
2026-05-11 14:10:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-from agent import create_agent
+from agent import create_chain
 
 st.set_page_config(page_title="AI Agent 🤖")
 
 st.title("🤖 LangChain + Groq AI Agent")
 
-agent = create_agent()
+chain = create_chain()
 
-user_input = st.text_input("Ask something:")
+user_input = st.text_input("Ask anything:")
 
 if st.button("Send"):
     if user_input:
-        response = agent.invoke({"input": user_input})
+        result = chain.invoke({"input": user_input})
         st.write("### 🤖 Response:")
-        st.write(response)
+        st.write(result)
